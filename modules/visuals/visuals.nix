1c3996465d93e90a7f1a4b92180df830b3571047
Nikita Shumeiko <nikita.shumeiko@vitalbio.com>
2024-03-04 22:26:09 -0500

    help

--- modules/visuals/visuals.nix
+++ modules/visuals/visuals.nix
@@ -83,7 +83,7 @@ in
           vim.opt.listchars:append({ space = "${cfg.indentBlankline.fillChar}" })
         ''}
 
-        require("ibl").setup {
+        require("indent_blankline").setup {
           enabled = true,
           char = "${cfg.indentBlankline.listChar}",
           show_end_of_line = ${boolToString cfg.indentBlankline.showEndOfLine},
